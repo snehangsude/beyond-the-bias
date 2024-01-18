a5c943c85fc5212beac29b955d01e724c2227827
Snehangsu De <desnehangsu@gmail.com>
2024-01-18 20:14:56 +0530

    EDIT: Remove variables

--- main.py
+++ main.py
@@ -4,25 +4,20 @@ from src import rss
 from src import _kafka_stream
 from src import _flink_sink
 
-from pyflink.common.serialization import SimpleStringSchema
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors import FlinkKafkaConsumer
-from pathlib import Path
 
 
 config_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))
 config = ConfigParser()
 config.read(f"{config_path[0]}/beyond-the-bias/config.ini")
-config_data= dict(config["default"])
-config_data.update(config['consumer'])
-
+config_data= dict(config["KAFKA"])
 
 robot = rss.RSSFeedFetcher(feed_url="https://rss.nytimes.com/services/xml/rss/nyt/World.xml")
 data = robot.fetch_feed()
 
 fc = rss.FeedCollector()
 ks = _kafka_stream.PushKafkaEvents(config=config_data)
-kafka_topic = 'rss-news'
+
+kafka_topic = config.get('KAKFA_SCHEMA', 'topic')
 key_generator = str(datetime.date.today())
 
 for json_string in fc.process_feed(data):
@@ -36,7 +31,7 @@ for json_string in fc.process_feed(data):
 
 
 
-processor = _flink_sink.FlinkProcessor(config_data, kafka_topic, jar_path="/home/cloudcraftz-11/Public/Python/Personal/beyond-the-bias/src/flink-sql-connector-kafka-1.16.0.jar")
+processor = _flink_sink.FlinkProcessor(config_data, kafka_topic, jar_path=config.get('FLINK', 'jar_path'))
 data_stream = processor.create_data_stream()
 # print(data_stream)
 
